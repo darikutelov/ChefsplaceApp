b066d30d58d880467bcab9ee9d14ea920c2e837d
darikutelov <dari@kutelov.com>
2024-08-21 11:36:56 +0300

    Admin screen

--- src/app/(tabs)/admin/index.tsx
+++ src/app/(tabs)/admin/index.tsx
@@ -1,15 +1,18 @@
-import { Text, SafeAreaView, Pressable } from "react-native"
-import { Href, Link } from "expo-router"
+import NavListItem from "@/src/components/ui/navigation/NavListItem"
+import { Text, SafeAreaView, View } from "react-native"
 
 export default function Admin() {
   return (
     <>
-      <SafeAreaView className='flex-1 justify-center items-center'>
-        <Pressable className='bg-gray-800 p-2 rounded'>
-          <Link href={"admin/categories" as Href}>
-            <Text className='text-white'>Categories</Text>
-          </Link>
-        </Pressable>
+      <SafeAreaView className='flex-1'>
+        <View className='p-5 gap-5'>
+          <Text className='text-3xl font-bold text-center mb-6'>Admin</Text>
+
+          <View className='divide-y'>
+            <NavListItem title={"Categories"} url={"admin/categories"} />
+            <NavListItem title={"Products"} url={"admin/products"} />
+          </View>
+        </View>
       </SafeAreaView>
     </>
   )
---
+++ src/components/ui/navigation/NavListItem.tsx
@@ -0,0 +1,21 @@
+import { Entypo } from "@expo/vector-icons"
+import { Href, Link } from "expo-router"
+import { View, Text, Pressable } from "react-native"
+
+type Props = {
+  title: string
+  url: string
+}
+
+export default function NavListItem({ title, url }: Props) {
+  return (
+    <Pressable className='py-5 w-full border-b border-gray-300'>
+      <Link href={`${url}` as Href}>
+        <View className='flex-row w-full justify-between'>
+          <Text className='text-lg'>{title}</Text>
+          <Entypo name='chevron-small-right' size={24} color='black' />
+        </View>
+      </Link>
+    </Pressable>
+  )
+}
